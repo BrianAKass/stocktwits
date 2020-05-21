c559c828c930a4150002af056a8aa04f64ab7059
BrianAKass <kasinator009@gmail.com>
2020-05-21 10:36:03 -0400

    made changes to cors

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const path = require("path");
 const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 3000;
-app.use(cors());
 
-app.get("/:symbol", function (req, res, next) {}); //will take whatever is in search bar
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.get("/:symbol", cors(corsOptions), function (req, res, next) {}); //will take whatever is in search bar
 
 app.use(express.static("client/build"));
 
-app.get("/*", function (req, res) {
+app.get("/*", cors(corsOptions), function (req, res) {
   res.sendFile(path.join(`${__dirname}/client`, "build", "index.html"));
 });
 
