1cebe3a869a69e49f019697424820ba6b5042bea
BrianAKass <kasinator009@gmail.com>
2020-05-21 10:48:27 -0400

    made changes to cors

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const corsOptions = {
   allowedHeaders: "*",
 };
 
-app.get("/:symbol", function (req, res, next) {}); //will take whatever is in search bar
+app.get("/:symbol", cors(corsOptions), function (req, res, next) {}); //will take whatever is in search bar
 
 app.use(express.static("client/build"));
 
-app.get("/*", function (req, res) {
+app.get("/*", cors(corsOptions), function (req, res) {
   res.sendFile(path.join(`${__dirname}/client`, "build", "index.html"));
 });
 
