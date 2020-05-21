b8a151bf7672f441075702a4c66b90781aaedbeb
BrianAKass <kasinator009@gmail.com>
2020-05-21 10:24:24 -0400

    made changes to cors

--- app.js
+++ app.js
@@ -2,20 +2,17 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const cors = require("cors");
-const PORT = process.env.PORT || 8080;
-app.options("*", cors());
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 
-const corsOptions = {
-  origin: "https://stocktwitscodingtest.herokuapp.com/", // where the request is coming from
-};
-app.get("/:symbol", cors(corsOptions), function (req, res, next) {}); //will take whatever is in search bar
+app.get("/:symbol", function (req, res, next) {}); //will take whatever is in search bar
 
 app.use(express.static("client/build"));
 
-app.get("/*", cors(corsOptions), function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(`${__dirname}/client`, "build", "index.html"));
 });
+
 app.listen(PORT, () => {
   console.log(`should be working. port ${PORT}`);
 });
